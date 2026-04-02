955e1ec5e59102e210bcfab5f1f191975bfbf8c1
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-03 00:45:09 +0530

    Created starter  page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-      <h1>Hello</h1>
+      <section className='main-section'>
+        <div className='heading'>
+          <h1>Work flows here.</h1>
+          <h2>Organize, prioritize, and crush your goals.</h2>
+        </div>
+        <div className='landing-page-buttons'>
+          <button className='btn landing-page-signin-btn'>SIGN IN</button>
+          <button className='btn landing-page-signup-btn'>SIGN UP</button>
+        </div>
+      </section>
     </>
   )
 }
