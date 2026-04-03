8f93e76e0381deb2b94459641a19a98517e4a53e
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-04 03:55:13 +0530

    Login UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import InputField from "../components/InputField";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+
+    const navigate = useNavigate();
+
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <>
-            <h2>Login Page</h2>
+            <section className="login-section">
+                <div className="login-box">
+                    <h3>Task Manager</h3>
+                    <h2>Log into your account</h2>
+                    <InputField
+                        type="text"
+                        minLength={8}
+                        maxLength={15}
+                        label="Username"
+                        placeHolder="Username"
+                        onChange={(e) => setUserName(e.target.value)}
+                        value={username}
+                    />
+                    <InputField
+                        type="password"
+                        minLength={8}
+                        maxLength={15}
+                        label="Password"
+                        placeHolder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+
+                    <p>Forgot Password?</p>
+                    <Button
+                        onClick={() => handleLogin()}
+                        label="LOGIN"
+                        className="btn-landing-page"
+
+                    />
+                    <p>Don't have an account?
+                        <span
+                            className="register-link"
+                            onClick={() => navigate('/register')}
+                        >  Register</span></p>
+                </div>
+            </section>
         </>
     )
 }
