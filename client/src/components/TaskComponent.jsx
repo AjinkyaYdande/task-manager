ba13597c549451ce00e365c82aafa34d611e64ea
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-04 17:18:41 +0530

    Added Task component

---
+++ client/src/components/TaskComponent.jsx
@@ -0,0 +1,240 @@
+import Button from '../components/Button';
+const TaskComoponent = () => {
+    const addNewTask = () => {
+        alert("Added new task");
+    }
+    return (
+        <>
+            <section className="task-section">
+                <Button
+                    label="Add new task"
+                    className="add-new-heading"
+                    icon="add-new"
+                    imgAlt="add-new"
+                    iconPath="/assets/icons/add-new-default.png"
+                    onClick={() => addNewTask()}
+                />
+                <div className='task-table-div'>
+                    <table border={0} className='task-table'>
+                        <thead>
+                            <tr>
+                                <th>Sr. No</th>
+                                <th>Task</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>MERN App</td>
+                                <td>WIP</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>JS</td>
+                                <td>Pending</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>DSA</td>
+                                <td>Done</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>MERN App</td>
+                                <td>WIP</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>JS</td>
+                                <td>Pending</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>DSA</td>
+                                <td>Done</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>MERN App</td>
+                                <td>WIP</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>JS</td>
+                                <td>Pending</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>DSA</td>
+                                <td>Done</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>MERN App</td>
+                                <td>WIP</td>
+                                <td>
+                                    <div>
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/pencil.png"
+                                        />
+                                        <Button
+                                            className="action-btn"
+                                            iconPath="/assets/icons/delete.png"
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+
+                        </tbody>
+
+                    </table>
+                    {/* <tfoot> */}
+                    <div className='tfooter-divider'></div>
+                    <div className='tfooter'>
+                        <div className='tfooter-child'>
+                            <Button
+                                className="t-btn-controls"
+                                iconPath="/assets/icons/left-arrow-default.png"
+                                imgAlt="previous-page"
+                            />
+
+                            <Button
+                                iconPath="/assets/icons/right-arrow-default.png"
+                                imgAlt="next-page"
+                                className="t-btn-controls" />
+                        </div>
+
+                        <div className='dropDownDiv'>
+
+                            <div className='dropDown'>
+                                <label className='t-footer-label'>Page size</label>
+                                <select>
+                                    <option>5</option>
+                                    <option>10</option>
+                                </select>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    {/* </tfoot> */}
+
+                </div>
+
+            </section >
+        </>
+    )
+}
+
+export default TaskComoponent;
