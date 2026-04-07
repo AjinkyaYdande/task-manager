4d5682129dbf8fca324cda402adc4738fdbf49be
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-07 23:55:14 +0530

    Added Server.js file

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors()); // allows frontend to call backend
+app.use(express.json()); // parse JSON request body
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Import routes
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+// Use routes
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+// Server start
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
