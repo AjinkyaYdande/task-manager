dfd72a238664781a20275c06b412081901e9eeb7
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-09 23:50:50 +0530

    Updated db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
 
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("DB Connection Error:", error.message);
-    process.exit(1); // stop server if DB fails
+  } catch(error) {
+    console.log("Connection failed",error.message);
+    process.exit(1);
   }
-};
+}
 
 module.exports = connectDB;
