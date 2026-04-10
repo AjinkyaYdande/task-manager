4c383a0b7de79a8cd77770278966f3f8fb18ebe5
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-11 01:20:48 +0530

    Created AuthRoutes apis

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User');
+
+router.post("/register", async (req, res) => {
+    try {
+        const { email, password } = res.body;
+
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            res.status(400).json({ message: "User already exist" });
+        }
+
+    }
+    catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+})
+
+router.get("/users", async (req, res) => {
+    try {
+        const users = await User.find().select('-password');
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+})
+
+
+module.exports = router;
+
