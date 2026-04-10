0abe2ca7077e257b4f2df0eb5e10bf002f923dab
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-11 01:21:56 +0530

    Integrated getall users API

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,29 @@
+import { useEffect } from "react";
 import GraphicalComponents from "../components/GraphicalComponents";
 import Header from "../components/Header";
 import TaskComoponent from "../components/TaskComponent";
 
 const Dashboard = () => {
+
+    const getAllUsers = () => {
+        try {
+            const API_URL = import.meta.env.VITE_APP_URL;
+            fetch(`${API_URL}/auth/users`)
+                .then(result => result.json)
+                .then(response => {
+                    console.log(response);
+                });
+            console.log("usEffect called");
+        } catch (error) {
+            alert("Server error");
+            console.log(error, "error");
+        }
+    }
+
+    useEffect(() => {
+        getAllUsers();
+    }, []);
+
     return (
         <>
 
