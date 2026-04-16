55c165115bce2d6eb74e4f6fdf546f8b8a4cb10f
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-16 23:55:34 +0530

    Created register API

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,13 +5,14 @@ const User = require('../models/User');
 
 router.post("/register", async (req, res) => {
     try {
-        const { email, password } = res.body;
+        const { email, password } = req.body;
 
         const userExist = await User.findOne({ email });
         if (userExist) {
-            res.status(400).json({ message: "User already exist" });
+            return res.status(400).json({ message: "User already exist" });
         }
-
+        const user = await User.create({ email, password });
+        res.status(201).json({ message: "User created successfully" });
     }
     catch (error) {
         res.status(500).json({ message: "Server Error" });
