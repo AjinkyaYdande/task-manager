95efc86bbd1e31bbd7f7a3935f0dc79c76b90ece
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-19 18:56:14 +0530

    Added some functionality

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,29 +1,82 @@
+import { useState } from "react";
 import { useModal } from "../context/ModalContext"
 import Button from "./Button"
+import InputField from "./InputField";
 
-function Modal() {
-    console.log("modal.jsx");
+function Modal({ refreshTask }) {
+    console.log(refreshTask, "refreshTask");
     const { isOpen, closeModal } = useModal();
 
+    const [task, setTask] = useState("");
+    const [status, setStatus] = useState("");
+
     if (!isOpen) return null;
-    else {
-        console.log("called");
+
+
+    const handleSubmit = () => {
+        addNewTask();
     }
+
+    const addNewTask = async () => {
+        try {
+            if (!task & !status) {
+                alert("Enter Task & Status");
+            } else if (!task) {
+                alert("Enter Task");
+            } else if (!status) {
+                alert("Enter Status");
+            } else {
+                const API_URL = import.meta.env.VITE_APP_URL
+                const response = await fetch(`${API_URL}/tasks/addTask`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ task, status })
+                })
+                const result = await response.json();
+                console.log(result, "result");
+                alert("Task added successfully");
+                // refreshTask();
+                closeModal();
+                setTask("");
+                setStatus("");
+
+            }
+        } catch (error) {
+            console.log(error, "error");
+        }
+    }
+
     return (
         <>
             <section className="modal-section">
                 <div className="modal-main" onClick={(e) => e.stopPropagation()}>
                     <div className="modal-title">
-                        Title
+                        Add New Task
                     </div>
                     <div className="modal-body">
-                        Body
+                        <InputField
+                            placeHolder="Task"
+                            label="Task"
+                            onChange={(e) => setTask(e.target.value)}
+                            value={task}
+                        />
+                        <InputField
+                            placeHolder="Status"
+                            label="Status"
+                            onChange={(e) => setStatus(e.target.value)}
+                            value={status}
+                        />
                     </div>
                     <div className="modal-footer">
-                        Footer
                         <div className="modal-action">
-                            <Button label="Submit" />
+                            <Button label="Submit"
+                                className="modal-submit"
+                                onClick={() => handleSubmit()}
+                            />
                             <Button label="Close"
+                                className="modal-close"
                                 onClick={closeModal} />
                         </div>
                     </div>
