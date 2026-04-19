d037b8409dfb98fce10a3fa4b8e6afda7124f634
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-19 18:58:28 +0530

    Added login API

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,16 +6,31 @@ const User = require('../models/User');
 router.post("/register", async (req, res) => {
     try {
         const { email, password } = req.body;
+        console.log(req.body, "register");
 
         const userExist = await User.findOne({ email });
         if (userExist) {
-            return res.status(400).json({ message: "User already exist" });
+            return res.status(400).json({ message: "User already exist", msgId: -1 });
         }
         const user = await User.create({ email, password });
-        res.status(201).json({ message: "User created successfully" });
+        res.status(201).json({ message: "User created successfully", msgId: 0 });
     }
     catch (error) {
         res.status(500).json({ message: "Server Error" });
+    }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userExist = User.findOne({ email });
+        if (!userExist) {
+            return res.status(404).json({ message: "User not registered", msgId: -1 });
+        } else {
+            return res.status(200).json({ message: "User found", msgID: 0 })
+        }
+    } catch (error) {
+        console.log(error, "error login");
     }
 })
 
