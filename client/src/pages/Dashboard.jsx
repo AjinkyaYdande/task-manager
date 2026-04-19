acf1224ca31dc375d9f764a8b78d08f2fdcc87aa
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-19 18:57:15 +0530

    Changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,27 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import GraphicalComponents from "../components/GraphicalComponents";
 import Header from "../components/Header";
 import TaskComoponent from "../components/TaskComponent";
+import Modal from "../components/Modal";
 
 const Dashboard = () => {
 
-    const getAllUsers = () => {
+    const [tableData, setTableData] = useState([]);
+
+    const getAllTasks = async () => {
         try {
             const API_URL = import.meta.env.VITE_APP_URL;
-            fetch(`${API_URL}/auth/users`)
-                .then(result => result.json)
-                .then(response => {
-                    console.log(response);
-                });
-            console.log("usEffect called");
+            const response = await fetch(`${API_URL}/tasks/allTasks`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+
+            const data = await response.json();
+            console.log(data, "response get all task");
+            setTableData(data)
+
         } catch (error) {
-            alert("Server error");
-            console.log(error, "error");
+            console.log(error, "error getAllTask");
         }
     }
 
     useEffect(() => {
-        getAllUsers();
+        getAllTasks();
     }, []);
 
     return (
@@ -30,8 +37,11 @@ const Dashboard = () => {
             <div>
                 <Header />
                 <div className="tasks-hero">
-                    <TaskComoponent />
+                    <TaskComoponent
+                        data={tableData}
+                    />
                     <GraphicalComponents />
+                    <Modal refreshTask={getAllTasks} />
                 </div>
             </div>
 
