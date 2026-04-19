378c203983ff09ad3a4abc3669603bb8b39e4a35
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-19 18:58:54 +0530

    Added task APIs and come changes

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -3,30 +3,32 @@ const router = express.Router();
 
 const Task = require('../models/Task');
 
-router.get("/", async (req, res) => {
+router.get("/allTasks", async (req, res) => {
     try {
-        const task = await Task.find({ userId: req.userId });
+        console.log(req.body, "reqqqq")
+        const task = await Task.find();
         res.json(task);
     } catch (error) {
         res.status(500).json({ message: "Server Error" });
     }
 })
 
-router.post("/", async (req, res) => {
+router.post("/addTask", async (req, res) => {
     try {
-        const { title } = req.body;
-
-        if (!title) {
+        const { task, status } = req.body;
+        console.log(req.body, "add task");
+        if (!task & !status) {
+            return res.status(400).json({ message: "Title and Status is required" });
+        } else if (!task) {
             return res.status(400).json({ message: "Title is required" });
+        } else if (!status) {
+            return res.status(400).json({ message: "Status is required" });
         }
-
-
-        const task = await Task.create({
-            // userId: req.user.id,
-            title,
-            completed: false
+        const taskRes = await Task.create({
+            task,
+            status
         })
-        res.json(task);
+        res.json(taskRes);
     } catch (error) {
         res.status(500).json({ message: "Server Error" });
     }
