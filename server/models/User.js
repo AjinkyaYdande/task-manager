62c2dc5cd3de2d224af5282d0b8c9d6ac86eb1ca
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-19 15:18:53 +0530

    Changes

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-    userName: { type: String },
-    passWord: { type: String },
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
 })
 
 module.exports = mongoose.model("User", userSchema);
